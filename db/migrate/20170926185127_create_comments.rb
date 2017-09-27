5f33214340c94f81ce0f02857d595f96a02f7397
Alex Erling <alex.erling@gmail.com>
2017-09-27 15:31:12 +0000

    Made small updates to checkpoint14

--- db/migrate/20170926185127_create_comments.rb
+++ db/migrate/20170926185127_create_comments.rb
@@ -1,11 +1,11 @@
-class CreateComments < ActiveRecord::Migration[5.1]
-  def change
-    create_table :comments do |t|
-      t.text :body
-      t.references :post, index: true
+class CreateComments < ActiveRecord::Migration[5.0]
+   def change
+     create_table :comments do |t|
+       t.text :body
+       t.references :post, index: true
 
-      t.timestamps null: false
-    end
-    add_foreign_key :comments, :posts
-  end
+       t.timestamps null: false
+     end
+     add_foreign_key :comments, :posts
+   end
 end
--- db/migrate/20170926191638_create_answers.rb
+++ db/migrate/20170926191638_create_answers.rb
@@ -1,10 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
-      t.references :question, foreign_key: true
+      t.references :question, index: true
       t.text :body
 
-      t.timestamps
+      t.timestamps null: false
     end
+    add_foreign_key :answers, :questions
   end
 end
