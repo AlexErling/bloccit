fb1612ee1fa52a28c27cc89f6488ebc1d64864b0
Alex Erling <alex.erling@gmail.com>
2017-09-28 19:14:54 +0000

    Completed checkpoint and assignment 18

---
+++ spec/models/advertisement_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Advertisement, type: :model do
+  let (:advertisment) {Advertisement.create!}
+  
+  describe "attributes" do
+      it "should respond to title" do
+          expect(advertisment).to respond_to(:title)
+      end
+      it "should respond to copy" do
+          expect(advertisment).to respond_to(:copy)
+      end
+      it "should respond to price" do
+          expect(advertisment).to respond_to(:price)
+      end
+  end
+      
+end
