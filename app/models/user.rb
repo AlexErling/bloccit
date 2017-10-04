8b78e1a6846986f3cc7ee2ec61ce3d3049f8a1f8
Alex Erling <alex.erling@gmail.com>
2017-10-04 23:48:30 +0000

    Finishing checkpoint 24

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,27 @@
 
 class User < ApplicationRecord
-  before_save { self.email = email.downcase if email.present? }
+  before_save { self.email = email.downcase }
+  before_save :format_name
+  
+  EMAIL_REGEX = /\A(\S+)@(.+)\.(\S+)\z/
+  
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
-            length: { minimum: 3, masimum: 254 }
+            length: { minimum: 3, masimum: 254 },
+            format: { with: EMAIL_REGEX }
   has_secure_password
   
-   def format_name
-       if name
+  def format_name
+      if name
            name_array = []
            name.split.each do |name_part|
-            name_array << name_part.capitalize
+             name_array << name_part.capitalize
            end
            self.name = name_array.join(" ")
-       end
-   end  
+      end
+  end  
 end
