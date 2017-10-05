4e60b51c289f740bc842225c44e63678d4137689
Alex Erling <alex.erling@gmail.com>
2017-10-05 16:11:42 +0000

    Made changes to get everything functioning again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@
      @user.email = params[:user][:email]
      @user.password = params[:user][:password]
      @user.password_confirmation = params[:user][:password_confirmation]
+     
    end
 
    def create
